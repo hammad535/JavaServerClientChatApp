544078b10bcb1ba6c3b6b714b4af05b414521035
hammad535 <hammadabbasi637@gmail.com>
2023-04-14 01:40:50 +0500

    Initial commit

---
+++ JavaServerClientChatApp/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.fxml.*;
+
+public class Main extends Application {
+    
+    Stage window;
+    String address;
+    int port;
+@Override
+    public void start(Stage primaryStage) throws Exception{
+
+}
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
